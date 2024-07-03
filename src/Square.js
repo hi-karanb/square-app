c6909fffb376d050354b2665160a946143299ab3
Karan <karanprince85@gmail.com>
2024-07-03 14:02:57 +0530

    s

--- src/Square.js
+++ src/Square.js
@@ -1,7 +1,14 @@
 function Square(){
     return(
         <>
+        <center>
         <h1>Hello World</h1>
+        <form>
+            <input type ="number" placeholder="enter any number"/>
+            <br></br>
+            <input type= "submit" value="find square"/>
+        </form>
+        </center>
         </>
     );
 } 
